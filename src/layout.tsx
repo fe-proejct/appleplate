5a59bc8a4799c6af41a332f6b95e06abc1984eca
developer.piglet <123jmk@naver.com>
2023-04-20 12:47:21 +0900

    중첩 div 삭제

--- src/layout.tsx
+++ src/layout.tsx
@@ -5,11 +5,11 @@ import Header from "./features/ui/Header";
 
 export default function Layout() {
   return (
-    <div>
+    <>
       <GlobalStyle />
       <Header />
       <Outlet />
       <Footer />
-    </div>
+    </>
   )
 }
