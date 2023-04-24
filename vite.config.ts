58c29290635b06b5ddef2a413b894641373262f2
Mik_a <ilemik14@gmail.com>
2023-04-24 16:56:25 +0900

    add kakao key

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,32 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
 // import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
-export default defineConfig(async() => {
+export default defineConfig(async ({ mode }: {
+  mode: string
+}) => {
   const mdx = await import('@mdx-js/rollup');
-  
+  const env = loadEnv(mode, process.cwd())
+
   return {
     optimizeDeps: {
       include: ['react/jsx-runtime'],
     },
-    plugins: [react(), mdx.default({ remarkPlugins: [] })],
+    plugins: [
+      react(),
+      mdx.default({ remarkPlugins: [] }),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: env.VITE_KAKAO_API,
+          }
+        }
+      }),
+
+    ],
   };
 });
