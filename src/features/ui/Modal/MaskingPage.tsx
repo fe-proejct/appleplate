b26c845fe13e36deccde57243fcb9514425cb0e8
developer.piglet <123jmk@naver.com>
2023-05-04 09:26:26 +0900

    modal 관련 컴포넌트 리팩토링

--- src/features/ui/Modal/MaskingPage.tsx
+++ src/features/ui/Modal/MaskingPage.tsx
@@ -1,59 +1,48 @@
+import { useEffect } from "react";
 import styled from "styled-components";
-import { ProfileModal } from "../../headerModal/ProfileModal";
-import { useEffect, useState } from "react";
 import { Modal } from "./Modal";
 
 export interface MakingProps {
-    openCheck:React.Dispatch<React.SetStateAction<Boolean>>;
-    closeCheck:Boolean;
-    name:string;
-    element:JSX.Element;
+  open: boolean;
+  element: JSX.Element;
+  onClose: () => void;
 }
 
 const StyleMaskingPage = styled.div`
-    height:100%;
-    width:100%;
-    background-color: var(--background-color, black);
-    opacity:0.5;
-    top:0;
-    z-index:6;
-    position:fixed;
-`
+  height: 100%;
+  width: 100%;
+  background-color: var(--background-color, black);
+  opacity: 0.5;
+  top: 0;
+  z-index: 6;
+  position: fixed;
+`;
 
 const StyleMaskingDiv = styled.div`
-    width:100%;
-    height:100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 
 export const MaskingPage = (props: MakingProps) => {
-    const [closeModalData, setCloseModalData] = useState<Boolean>(props.closeCheck);
-    const {openCheck, closeCheck} = props
-    const closeModal = () => {
-        setCloseModalData(!closeModalData);
-        openCheck(!closeModalData);
-    }
+  const { open, onClose } = props;
 
-    //스크롤 막기
-    useEffect(() => {
-        document.body.style.cssText = `
+  //스크롤 막기
+  useEffect(() => {
+    document.body.style.cssText = `
           position: fixed; 
           top: -${window.scrollY}px;
           width: 100%;`;
-        return () => {
-          const scrollY = document.body.style.top;
-          document.body.style.cssText = '';
-          window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-        };
-      }, []);
-    
-    return (
-        <StyleMaskingDiv>
-            { closeCheck === closeModalData && (<StyleMaskingPage onClick={closeModal}/>) }
-            {
-                closeCheck === closeModalData && (
-                    <Modal value={props}/>
-                )
-            }
-        </StyleMaskingDiv>
-    );
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
+  return (
+    <StyleMaskingDiv>
+      {open && <StyleMaskingPage onClick={onClose} />}
+      {open && <Modal {...props} />}
+    </StyleMaskingDiv>
+  );
 };
